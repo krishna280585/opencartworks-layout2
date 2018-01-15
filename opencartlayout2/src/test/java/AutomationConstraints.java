592baae002146736221ad8547c997a506f3dfa8c
krishna280585 <kk.madhuri@gmail.com>
2018-01-15 19:17:19 +0000

    new changes are done

--- opencartlayout2/src/test/java/AutomationConstraints.java
+++ opencartlayout2/src/test/java/AutomationConstraints.java
@@ -1,7 +1,8 @@
 public class AutomationConstraints {
 public static final String BROWSER="chrome";
 public static final String  URL="http://opencart.opencartworks.com/themes/so_emarket/layout2";
-
+    public static String Chrome="yes";
+    public static String Firefox=" ";
     public static final String OS="Mac OS";
     public static final int TIMEOUT=30;
 }
