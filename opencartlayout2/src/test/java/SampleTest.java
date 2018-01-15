0c26ffe5cf76bf2707258bc4048400fbd9ac0431
krishna280585 <kk.madhuri@gmail.com>
2018-01-15 19:22:30 +0000

    new changes are done

--- opencartlayout2/src/test/java/SampleTest.java
+++ opencartlayout2/src/test/java/SampleTest.java
@@ -3,7 +3,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"html:target/cucu","json:target/cucuber.json"})
+@CucumberOptions(plugin={"html:target/cucu","json:target/cucuber.json"},
+        features="src/test/resources/featurefiles/sample")
 public class SampleTest {
 
 }
